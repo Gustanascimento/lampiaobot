da7e973bd5d6b89c3aa91eaa0f83931502ec64ed
Gustanascimento <gustavo.nascimento@turivius.com>
2023-04-11 00:25:11 -0300

    removing unnecessary code

--- Pinecone.py
+++ Pinecone.py
@@ -28,24 +28,15 @@ class PineconeEmbedder():
         reference_embedding = openai.Embedding.create(input=original_prompt, engine=embedding_model)['data'][0]['embedding']
 
         comparison_embeddings = [openai.Embedding.create(input=phrase, engine=embedding_model)['data'][0]['embedding'] for phrase in comparison_phrases]
-        
-        # Calcular a similaridade de cosseno entre o embedding de referência e os embeddings de comparação
+
         similarity_scores = cosine_similarity(np.array(reference_embedding).reshape(1, -1), np.array(comparison_embeddings))
 
         scores = list(similarity_scores[0])
-
-        # Encontrar o índice da frase mais similar
-        most_similar_index = np.argmax(similarity_scores)
-
-        # Recuperar a frase mais similar de comparison_phrases
-        most_similar_phrase = comparison_phrases[most_similar_index]
 
         results = []
 
         for player, phrase, pontuacao in zip(usernames, comparison_phrases, scores):
             results.append((player, phrase, pontuacao))
-
-        print("Frase mais similar: ", most_similar_phrase)
 
         results = sorted(results, key=lambda x: x[2], reverse=True)
         return results
